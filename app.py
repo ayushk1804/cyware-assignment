28cd18a47b6d4de06fe5730680e174adb788cffa
aayush-kumar-bm-ai <aayush.kumar@brightmoney.co>
2022-10-15 10:15:03 +0530

    🔧 Add a health_check api
    Signed-off-by: aayush-kumar-bm-ai <aayush.kumar@brightmoney.co>

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask
 
+IS_HEALTHY = True
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -7,5 +9,12 @@ def hello_team():
     '''I know, its a simple program ;)'''
     return 'Hi Team'
 
+@app.route('/health_check')
+def health_check():
+    if IS_HEALTHY:
+        return "Healthy",200
+    else:
+        return "Unhealthy",502
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8000)
+    app.run(host='0.0.0.0', port=15102)
